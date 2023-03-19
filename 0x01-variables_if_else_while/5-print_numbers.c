e4787789967e8849a707d248ab24b18dd896743d
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-03-19 10:48:35 +0000

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ **/
+int main(void)
+{
+	/*My code is here*/
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
