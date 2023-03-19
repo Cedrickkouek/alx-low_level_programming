b730a6646f300e9667b35f036c55893c0d8f9015
Cedrickkouek <cedrickkouek@gmail.com>
2023-03-19 14:13:53 -0700

    Glow code

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,23 +7,25 @@
 int main(void)
 {
 	int a, b, c, d;
-
+	
 	for (a = 0; a < 1000; a++)
 	{
 		b = a / 100;/*Hundredth */
 		c = (a / 10) % 10;/* Tens */
 		d = a % 10;/* Units */
+		
 		if ((b < c) && (c < d))
 		{
-			putchar(b + '0');
-			putchar(c + '0');
-			putchar(d + '0');
-			if (a < 700)
-			{
-				putchar(44);
-				putchar(32);
+			putchar(b +'0');
+				putchar(c + '0');
+				putchar(d + '0');
+				
+				if (a < 700)
+				{
+					putchar(44);
+					putchar(32);
+				}
 			}
-		}
 	}
 	putchar('\n');
 	return (0);
