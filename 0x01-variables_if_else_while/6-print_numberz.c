32d66a21fb7249fd572e255f04b42c6f5f22b60e
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-03-19 11:09:34 +0000

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	/*Your code*/
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+	return (0);
+}
