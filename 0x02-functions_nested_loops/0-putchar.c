e4a5361b8728a344c6f0b231ad91d8571bbdf3bc
Cedrickkouek <cedrickkouek@gmail.com>
2023-03-22 16:20:58 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 /**
- *main - entry point
- *
- *Return: Always 0 (Success)
- **/
+ * main - entry point
+ * Description: 'prints _putchar to stdout'
+ * Return: Always 0 (Success)
+ */
 int main(void)
+	/*Putchar return char*/
 {
-	printf("_putchar\n");
+	/*This is the putchar function called adn past characters as parameters*/
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	/*Returns char*/
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
