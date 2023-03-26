a28bddf2e944f6c7c621c402d4f383e6bf96057c
Cedrickkouek <cedrickkouek@gmail.com>
2023-03-25 18:03:38 -0700

    yessir

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,27 +5,21 @@
   */
 int main(void)
 {
-	unsigned long count, i, j, k;
+	unsigned long count, sum, i, j, k;
 
 	i = 0;
 	j = 1;
-	for (count = 0; count <= 4000000; count++)
+	sum = 0;
+	for (count = 0; count <= 100; count++)
 	{
 		k = i + j;
 		i = j;
 		j = k;
-		
-		if (k % 2 == 0 )
+		if (k % 2 == 0 && k <= 4000000)
 		{
-			printf("%lu", k);
-		}
-
-		if (count == 4000000)
-			putchar('\n');
-		else
-		{
-			printf(", ");
+			sum += k;
 		}
 	}
+	printf("%lu", sum);
 	return (0);
 }
