14d7c59f39420d07ebef568cf8459beab47cc034
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-04-02 21:49:36 +0000

    multiplication ; git push
    qsc
    qsd
    qsd
    
    ls<
    
    cd ..
     dc
    
    sdk

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - a function that computes the product of a and b
+ * @a: first input
+ * @b: second input
+ * Return: Always c
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
