c5cd21b40ac3309dd8ab89c386579af99615b0ac
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-04-11 13:48:57 +0000

    Print all arguments on seperate lines

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all parsed arguments
+ * @argv: array of arguments
+ * @argc: number of characters
+ * Return: Always 0.
+ **/
+int main(int argc, char *argv[])
+{
+	int i, n;
+
+	n = argc -1;
+	i = 0;
+	while (i <= n && *argv[i] != '\0')
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
