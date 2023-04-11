9f33d3c36db7b51a24b62e9cebe31fb7c5b7aa32
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-04-11 13:36:11 +0000

    that is it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments parsed in.
+ * @argv: array of pointers to string of arguments
+ * @argc: number of arguments
+ * Return: Always 0.
+ **/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
