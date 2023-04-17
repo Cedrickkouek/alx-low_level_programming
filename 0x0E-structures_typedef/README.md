219c3b48c7879bd3291d770cc693f91341d84f86
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-04-17 20:35:34 +0000

    Propre Code

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+here we working on typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOGHEADER
+#define DOGHEADER
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - this is a dog's structure.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
