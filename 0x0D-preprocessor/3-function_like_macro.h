7c59d28a4b0c75d4437d944bd324821a0633e0aa
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-04-23 09:58:34 +0000

    Absolute Task

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,6 +5,6 @@
  * Description: Header File that define a function-like macro ABS(x)
  * that computes the absolute value of a number x.
  */
-#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
 #endif
