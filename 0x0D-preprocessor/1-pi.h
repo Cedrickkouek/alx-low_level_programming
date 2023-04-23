9946f446bdcf5a1f7252cf4dc0fb04c21498be62
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-04-23 09:17:26 +0000

    PI TASK

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,10 @@
+#ifndef PI_H
+#define PI_H
+/**
+ * Header File: 1-pi.h
+ * Description: Header File that defines A macro named PI
+ *  As an abbreviation for the token 3.14159265359
+ */
+#define PI 3.14159265359
+
+#endif
