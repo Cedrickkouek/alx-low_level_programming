f77e97f137e1c70797921eceb2e7b08a532695e8
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-04-23 09:07:58 +0000

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/**
+ * Header file: 0-object_like_macro.h
+ * Description: Header File that defines a macro named
+ *    SIZE as an abbreviation for the token 1024
+ */
+#define SIZE 1024
+
+#endif
