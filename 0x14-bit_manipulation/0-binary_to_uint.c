5a3515f3c1cc0cb8bcf27dd994c6365071ea4a1b
cedrickkouek <cedrickkouek@gmail.com>
2023-07-07 01:29:59 +0100

    Yes

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary
+ * Return: unsigned int.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int y = 0;
+	int len, base = 1;
+
+	if (!b)
+	{
+		return (0);
+	}
+	while (b[len])
+	{
+		len++;
+	}
+
+	while(len)
+	{
+		if
+		(b[len - 1] != '0' && b[len - 1] != '1')
+		{
+			return (0);
+		}
+		else if (b[len - 1] == '1')
+		{
+			y = y + base;
+		}
+		base = base * 2;
+		len = len - 1;
+	}
+	return (y);
+}
