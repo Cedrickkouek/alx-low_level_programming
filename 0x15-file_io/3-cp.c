748aaf5b76d52e15e8112043f06791447272ccb9
cedrickkouek <cedrickkouek@gmail.com>
2023-07-10 23:00:09 +0100

    yeah sir

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,54 +1,7 @@
 #include "main.h"
-#define buffer_size 1024
-/**
- * check_98 - checks error 98
- * @ff: the value to be checked
- * @buffer: the buffer
- * @argv: argument
- * Return: nothing
- */
-void check_98(int ff, char *buffer, char *argv)
-{
-	if (ff < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv);
-		free(buffer);
-		exit(98);
-	}
-}
-/**
- * check_99 - checks error 99
- * @ff: the value to be checked
- * @buffer: the buffer
- * @argv: argument
- * Return: nothing
- */
+#include <stdio.h>
+#include <stdlib.h>
 
-void check_99(int ff, char *buffer, char *argv)
-{
-	if (ff < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
-		free(buffer);
-		exit(99);
-	}
-}
-/**
- * check_100 - checks error 100
- * @ff: the value to be checked
- * @buffer: the buffer
- * @argv: argument
- * Return: nothing
- */
-void check_100(int ff, char *buffer)
-{
-	if (ff < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", ff);
-		free(buffer);
-		exit(100);
-	}
-}
 /**
  * main - program that copies the content of a file to another
  * @argc: argument counter
@@ -57,52 +10,47 @@ void check_100(int ff, char *buffer)
  */
 int main(int argc, char *argv[])
 {
-	int sf, ff, cp1, cp2;
-	char *buffer;
+	int file_from, file_to;
+	int num1 = 1024, num2 = 0;
+	char buf[1024];
 
-	/*Check num of arguments passed in*/
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-
-	/*Creating the space where copied text will be stored*/
-	buffer = malloc(sizeof(char) * buffer_size);
-	if (!buffer)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
-		return (0);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-
-	/*Open source file*/
-	sf = open(argv[1], O_RDONLY);
-
-	/*Check if there is any error with sf*/
-	check_98(sf, buffer, argv[1]);
-
-	/*open destination file*/
-	ff = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-
-	/*Check the destination file */
-	check_99(ff, buffer, argv[2]);
-
-	do {
-		cp1 = read(sf, buffer, buffer_size);
-		if (cp1 == 0)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from), exit(99);
+	}
+	while (num1 == 1024)
+	{
+		num1 = read(file_from, buf, 1024);
+		if (num1 == -1)
 		{
-			return (0);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 		}
-		else
+		num2 = write(file_to, buf, num1);
+		if (num2 < num1)
 		{
-			check_98(sf, buffer, argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
-		cp2 = write(ff, buffer, cp1);
-		check_99(cp2, buffer, argv[2]);
-	} while (cp2 >= buffer_size);
-	cp1 = close(sf);
-	check_100(cp1, buffer);
-	cp1 = close(ff);
-	check_100(cp1, buffer);
-	free(buffer);
+	}
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	}
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,9 +5,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
