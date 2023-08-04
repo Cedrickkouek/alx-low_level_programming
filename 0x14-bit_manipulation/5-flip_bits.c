c7c02f0905c4dfa9ebab7a9c0293af388fc19d58
cedrickkouek <cedrickkouek@gmail.com>
2023-08-04 23:44:05 +0100

    yess

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * flip_bits - counts the number of bits to flip to go from one number to
+ * the next
+ * @n: the first number
+ * @m: the second number
+ * Return: if okay
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int mirror;
+	int numofbits;
+
+	mirror = n ^ m;
+	numofbits = 0;
+	while (mirror)
+	{
+		if (mirror & 1)
+		{
+			numofbits++;
+		}
+		mirror >>= 1;
+	}
+
+	return numofbits;
+}
