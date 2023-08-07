80fa10ddbd0e777acfdb5cc9d04c581adb42c108
cedrickkouek <cedrickkouek@gmail.com>
2023-08-07 23:42:55 +0100

    grow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,14 +9,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int fs, num, wr;
 
-	if (filename == NULL)
+	if (!filename)
 	{
 		return (-1);
 	}
 	
 	fs = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (text_content == NULL)
+	if (!text_content)
 	{
 		text_content = "";
 	}
