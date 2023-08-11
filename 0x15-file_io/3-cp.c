f80e9c9f0043419676a32f6021a22b8d967a520c
cedrickkouek <cedrickkouek@gmail.com>
2023-08-11 22:27:12 +0100

    superrr

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	destination = open(argv[2], O_WRONLY | O_CREAT, 0664);
+	destination = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (destination == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
