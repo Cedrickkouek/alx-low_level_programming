e58f375cd6b4c9b55daee39f17d808a9b8f54be7
cedrickkouek <cedrickkouek@gmail.com>
2023-09-28 21:41:17 +0100

    improved again

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -4,7 +4,7 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- * Return: Nothing (void). 
+ * Return: Nothing (void).
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ char *_strchr(char *s, char c);
 char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 
 #endif
