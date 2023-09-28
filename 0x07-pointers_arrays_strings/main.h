954120790ad76412cbde6eaf4a58406d5068933a
cedrickkouek <cedrickkouek@gmail.com>
2023-09-28 21:37:18 +0100

    yep

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
 
 #endif
