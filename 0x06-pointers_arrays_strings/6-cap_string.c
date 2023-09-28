97ac210362afc942b661fe787f026e2eb990f81e
cedrickkouek <cedrickkouek@gmail.com>
2023-09-28 22:28:15 +0100

    yes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,36 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- *cap_string - capitalizes most iif the words in a string.
- *@s: analized string.
- *Return: String with all words capitalized.
+ * cap_string - capitalizes most iif the words in a string.
+ * @s: analized string.
+ * Return: String with all words capitalized.
  */
 char *cap_string(char *s)
 {
-	int i = 0;
-	int new_word = 1;
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (s[i] != '\0')
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'
-			|| s[i] == ',' || s[i] == ';' || s[i] == '.' ||
-				s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' ||
-					s[i] == ')' || s[i] == '{' || s[i] == '}')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			new_word = 1;
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
+			}
 		}
-		else if (new_word == 1)
-		{
-			s[i] = toupper(s[i]);
-			new_word = 0;
-		}
-
 		i++;
 	}
-
-	return (s);
+	return (0);
 }
